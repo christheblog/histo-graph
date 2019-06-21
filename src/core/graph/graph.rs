228fed4b5ef47ee83e4001b78d28d1fec1ca3445
David Peklak <dpeklak@gmail.com>
2019-06-21 13:03:27 +0200

    impl From<(VertexId, VertexId)> for Edge

--- src/core/graph/graph.rs
+++ src/core/graph/graph.rs
@@ -1,23 +1,23 @@
+
+/// Identifies and represents a vertex in a graph.
 #[derive(PartialEq, Eq, Clone, Copy, Hash, PartialOrd, Ord, Debug)]
 pub struct VertexId(pub u64);
 
+/// Represents a directed edge in a graph.
+/// The first `VertexId` is the start of the edge, the second `VertexId` is its end.
 #[derive(PartialEq, Eq, Clone, Copy, Hash, PartialOrd, Ord, Debug)]
 pub struct Edge(pub VertexId, pub VertexId);
 
 impl Edge {
 
-    pub fn from_pair(vertices: (VertexId, VertexId)) -> Edge {
-        let (v1, v2) = vertices;
-        Edge(v1, v2)
-    }
-
-    pub fn as_pair(&self) -> (VertexId, VertexId) {
-        let Edge(v1, v2) = *self;
-        (v1,v2)
-    }
-
     pub fn reverse(&self) -> Edge {
         let Edge(v1, v2) = *self;
         Edge(v2, v1)
+    }
+}
+
+impl From<(VertexId, VertexId)> for Edge {
+    fn from(pair: (VertexId, VertexId)) -> Edge {
+        Edge(pair.0, pair.1)
     }
 }
