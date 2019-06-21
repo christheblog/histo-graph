ab0d7830fd5bcaa69859b9dcce15bffcbfecc9d8
David Peklak <dpeklak@gmail.com>
2019-06-21 23:48:45 +0200

    add more documentation

--- src/core/graph/command.rs
+++ src/core/graph/command.rs
@@ -1,17 +1,28 @@
 use crate::core::graph::graph::{VertexId, Edge};
 use crate::core::graph::directed_graph::DirectedGraph;
 
+/// A command to manipulate a [`DirectedGraph`]
+///
+/// [`DirectedGraph`]: ../directed_graph/struct.DirectedGraph.html
 #[derive(PartialEq, Eq, Clone, Copy, Hash, Debug)]
 pub enum GraphCommand {
+
+    /// Adds a vertex to a graph.
     AddVertex(VertexId),
+
+    /// Removes a vertex from a graph.
     RemoveVertex(VertexId),
+
+    /// Adds an edge to a graph.
     AddEdge(VertexId, VertexId),
+
+    /// Removes an edge from a graph.
     RemoveEdge(VertexId, VertexId),
 }
 
-// Not sure this should be here ...
 impl GraphCommand {
-    /// return a command to revert the one provided in argument
+
+    /// Returns a command that reverts the provided `command`.
     pub fn revert(command: GraphCommand) -> GraphCommand {
         use GraphCommand::*;
         match command {
@@ -22,25 +33,25 @@ impl GraphCommand {
         }
     }
 
-    /// Apply a list of commands on a MutableGraph
+    /// Applies a vector of commands to a MutableGraph
     pub fn apply_commands(commands: Vec<GraphCommand>, graph: &mut DirectedGraph) -> () {
         for command in commands.iter() {
-            GraphCommand::apply_command(*command, graph);
+            command.apply_to(graph);
         }
     }
 
-    /// Apply a command on a MutableGraph
-    pub fn apply_command(command: GraphCommand, graph: &mut DirectedGraph) -> bool {
+    /// Applies the command to a MutableGraph.
+    pub fn apply_to(&self, graph: &mut DirectedGraph) -> bool {
         use GraphCommand::*;
-        match command {
-            AddVertex(v) => graph.add_vertex(v),
-            RemoveVertex(v) => graph.remove_vertex(v),
-            AddEdge(v1, v2) => graph.add_edge(Edge(v1, v2)),
-            RemoveEdge(v1, v2) => graph.remove_edge(Edge(v1, v2)),
+        match self {
+            AddVertex(v) => graph.add_vertex(*v),
+            RemoveVertex(v) => graph.remove_vertex(*v),
+            AddEdge(v1, v2) => graph.add_edge(Edge(*v1, *v2)),
+            RemoveEdge(v1, v2) => graph.remove_edge(Edge(*v1, *v2)),
         }
     }
 
-    /// Extracts a graph as a list of commands
+    /// Extracts a graph as a vector of commands.
     pub fn as_commands(graph: &DirectedGraph) -> Vec<GraphCommand> {
         use GraphCommand::*;
         let mut res: Vec<GraphCommand> = vec![];
