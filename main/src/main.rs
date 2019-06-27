ba053e03f04c51ca82a5eb67480817f3826cb2a2
David Peklak <dpeklak@gmail.com>
2019-06-27 17:53:12 +0200

    serialize a graph in main

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,21 +1,34 @@
-use histo_graph_core::graph::directed_graph::DirectedGraph;
-
+use serde::Serialize;
 use serde_json;
-use histo_graph_core::graph::graph::{VertexId, Edge};
+
+use histo_graph_core::graph::directed_graph::DirectedGraph;
+use histo_graph_core::graph::graph::{Edge, VertexId};
 use histo_graph_core::util::b_tree_bag::BTreeBag;
 
+#[derive(Serialize)]
+struct DirectedGraphSer {
+    vertices: Vec<u64>,
+    edges: Vec<(u64, u64)>,
+}
+
 fn main() -> Result<(), serde_json::error::Error>{
-    let mut bag: BTreeBag<Edge> = BTreeBag::new();
+    let mut graph = DirectedGraph::new();
+    graph.add_edge(Edge(VertexId(0), VertexId(1)));
+    graph.add_edge(Edge(VertexId(0), VertexId(2)));
+    graph.add_edge(Edge(VertexId(0), VertexId(0)));
 
-    bag.insert(Edge(VertexId(0), VertexId(1)));
-    bag.insert(Edge(VertexId(1), VertexId(0)));
+    let ser = DirectedGraphSer {
+        vertices: graph
+            .vertices()
+            .map(|&VertexId(id)| id)
+            .collect(),
+        edges: graph
+            .edges()
+            .map(|&Edge(VertexId(id_1), VertexId(id_2))| (id_1, id_2))
+            .collect()
+    };
 
-    let vec: Vec<(u64, u64)> = bag
-        .iter()
-        .map(|&Edge(VertexId(id1), VertexId(id2))| (id1, id2))
-        .collect();
-
-    let str = serde_json::to_string(&vec)?;
+    let str = serde_json::to_string(&ser)?;
 
     println!("{}", str);
 
