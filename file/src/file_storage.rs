dad8934229bdecbd1702aef89c0f1122ced69aa7
David Peklak <dpeklak@gmail.com>
2019-07-05 07:28:52 +0200

    implement  write_all_vertices_to_files
    without path

--- file/src/file_storage.rs
+++ file/src/file_storage.rs
@@ -5,39 +5,60 @@ use data_encoding::HEXLOWER;
 
 use std::path::Path;
 use futures::future::Future;
-use tokio_fs::CreateDirFuture;
 
 struct File {
     content: Vec<u8>,
     name: String,
 }
 
-fn vertex_to_file(vertex_id: &VertexId) -> File {
-    // serialize the vertex_id
-    let content: Vec<u8> = bincode::serialize(&vertex_id.0).unwrap();
+pub struct Hash([u8; 32]);
 
-    // create a SHA256 hash of the serialized vertex_id
-    let mut context = Context::new(&SHA256);
-    context.update(&content);
-    let digest = context.finish();
-    let name = HEXLOWER.encode(digest.as_ref());
-
-    File {
-        content,
-        name,
+impl Hash {
+    fn to_string(&self) -> String {
+        HEXLOWER.encode(&self.0)
     }
 }
 
-fn create_dir() {
-    let path = Path::new("./foo");
-    let f = tokio_fs::create_dir(path);
-    let f = f.and_then(| _ | {
-        println!("created directory");
-        Ok(())
-    });
-    let f = f.map_err(|_| eprintln!("Error"));
+impl<T> From<T> for Hash
+    where T: AsRef<[u8]> {
+    fn from(content: T) -> Hash {
+        let mut context = Context::new(&SHA256);
+        context.update(content.as_ref());
+        let digest = context.finish();
+        let mut hash: [u8; 32] = [0u8; 32];
+        hash.copy_from_slice(digest.as_ref());
 
-    tokio::run(f);
+        Hash(hash)
+    }
+}
+
+fn vertex_to_file(vertex_id: &VertexId) -> File {
+    // serialize the vertex_id
+    let content: Vec<u8> = bincode::serialize(&vertex_id.0).unwrap();
+    let hash: Hash = (&content).into();
+
+    File {
+        content,
+        name: hash.to_string(),
+    }
+}
+
+fn write_file_in_dir(dir_path: &Path, file: File) -> impl Future {
+    let path = dir_path.join(&file.name);
+    tokio_fs::write(path, file.content)
+}
+
+
+
+fn write_all_vertices_to_files<I>(i: I) -> impl Future
+    where I: Iterator,
+          <I as Iterator>::Item: AsRef<VertexId>
+{
+    let futs = i
+        .map(| v | vertex_to_file(v.as_ref()))
+        .map(| f | write_file_in_dir(Path::new("some_path") , f)) ;
+
+    futures::future::join_all(futs)
 }
 
 fn write_file(file: File) -> impl Future {
@@ -55,13 +76,12 @@ mod test {
     use histo_graph_core::graph::graph::VertexId;
     use super::File;
     use super::vertex_to_file;
-    use super::create_dir;
     use super::write_file;
     use futures::future::Future;
 
     #[test]
     fn test_hash() {
-        let File{content, name} = vertex_to_file(&VertexId(27));
+        let File{content: _, name} = vertex_to_file(&VertexId(27));
 
         assert_eq!(name, "4d159113222bfeb85fbe717cc2393ee8a6a85b7ce5ac1791c4eade5e3dd6de41")
     }
