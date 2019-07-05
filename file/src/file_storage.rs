2118b654b5c02600f83064066777b10bbde3b9b0
David Peklak <dpeklak@gmail.com>
2019-07-05 18:47:42 +0200

    save files in parallel

--- file/src/file_storage.rs
+++ file/src/file_storage.rs
@@ -3,8 +3,12 @@ use histo_graph_core::graph::graph::VertexId;
 use ring::digest::{Context, SHA256};
 use data_encoding::HEXLOWER;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use futures::future::Future;
+use std::{
+    borrow::Borrow,
+    io
+};
 
 struct File {
     content: Vec<u8>,
@@ -43,20 +47,19 @@ fn vertex_to_file(vertex_id: &VertexId) -> File {
     }
 }
 
-fn write_file_in_dir(dir_path: &Path, file: File) -> impl Future {
+fn write_file_in_dir(dir_path: &Path, file: File) -> impl Future<Error = io::Error> {
     let path = dir_path.join(&file.name);
     tokio_fs::write(path, file.content)
 }
 
 
-
-fn write_all_vertices_to_files<I>(i: I) -> impl Future
+fn write_all_vertices_to_files<I>(path: PathBuf, i: I) -> impl Future<Item=Vec<()>, Error = io::Error>
     where I: Iterator,
-          <I as Iterator>::Item: AsRef<VertexId>
+          <I as Iterator>::Item: Borrow<VertexId>
 {
     let futs = i
-        .map(| v | vertex_to_file(v.as_ref()))
-        .map(| f | write_file_in_dir(Path::new("some_path") , f)) ;
+        .map(| v | vertex_to_file(v.borrow()))
+        .map(move | f | write_file_in_dir(path.as_ref(), f).and_then(| _ | Ok(()))) ;
 
     futures::future::join_all(futs)
 }
@@ -74,10 +77,12 @@ fn write_file(file: File) -> impl Future {
 #[cfg(test)]
 mod test {
     use histo_graph_core::graph::graph::VertexId;
-    use super::File;
-    use super::vertex_to_file;
-    use super::write_file;
+    use super::{File,
+                vertex_to_file,
+                write_file,
+                write_all_vertices_to_files};
     use futures::future::Future;
+    use std::path::{Path, PathBuf};
 
     #[test]
     fn test_hash() {
@@ -93,6 +98,37 @@ mod test {
         let f = write_file(file)
             .map(| _ | ())
             .map_err(| _ | eprintln!("Error"));
+
+        tokio::run(f);
+    }
+
+    /*fn the_future() -> impl Future<Item = (), Error = ()> {
+        let vertices = vec!{VertexId(1), VertexId(2), VertexId(3), VertexId(4)};
+        let iter = vertices.iter();
+
+        let path: PathBuf = Path::new("foo/").into();
+
+        let f = write_all_vertices_to_files(path, iter);
+
+        let f = f
+            .and_then(| _ | { println!("Done"); Ok(()) })
+            .map_err(| _ | eprintln!("Error"));
+
+        f
+    }*/
+
+    #[test]
+    fn test_write_vertices() {
+        let vertices = vec!{VertexId(1), VertexId(2), VertexId(3), VertexId(4)};
+
+        let path: PathBuf = Path::new("foo/").into();
+
+        let f = write_all_vertices_to_files(path, vertices.into_iter());
+
+        let f = f
+            .and_then(| _ | { println!("Done"); Ok(()) })
+            .map_err(| _ | eprintln!("Error"));
+
 
         tokio::run(f);
     }
