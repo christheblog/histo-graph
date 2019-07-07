4f7a9357b67d8e0fe0d1bebce485b46c987a633f
David Peklak <dpeklak@gmail.com>
2019-07-07 14:11:20 +0200

    read and write vertex

--- file/src/file_storage.rs
+++ file/src/file_storage.rs
@@ -9,12 +9,9 @@ use std::{
     borrow::Borrow,
     io
 };
+use histo_graph_core::graph::directed_graph::DirectedGraph;
 
-struct File {
-    content: Vec<u8>,
-    name: String,
-}
-
+#[derive(Clone, Copy)]
 pub struct Hash([u8; 32]);
 
 impl Hash {
@@ -36,6 +33,11 @@ impl<T> From<T> for Hash
     }
 }
 
+struct File {
+    content: Vec<u8>,
+    hash: Hash,
+}
+
 fn vertex_to_file(vertex_id: &VertexId) -> File {
     // serialize the vertex_id
     let content: Vec<u8> = bincode::serialize(&vertex_id.0).unwrap();
@@ -43,35 +45,50 @@ fn vertex_to_file(vertex_id: &VertexId) -> File {
 
     File {
         content,
-        name: hash.to_string(),
+        hash,
     }
 }
 
+fn file_to_vertex(file: &File) -> bincode::Result<VertexId> {
+    let id: u64 = bincode::deserialize(file.content.as_ref())?;
+    Ok(VertexId(id))
+}
+
 fn write_file_in_dir(dir_path: &Path, file: File) -> impl Future<Error = io::Error> {
-    let path = dir_path.join(&file.name);
+    let path = dir_path.join(&file.hash.to_string());
     tokio_fs::write(path, file.content)
 }
 
+fn read_file_in_dir(dir_path: &Path, hash: Hash) -> impl Future<Item = File, Error = io::Error> {
+    let path = dir_path.join(hash.to_string());
+    tokio_fs::read(path)
+        .map( move |content| File {
+            content,
+            hash
+        })
+}
 
 fn write_all_vertices_to_files<I>(path: PathBuf, i: I) -> impl Future<Item=Vec<()>, Error = io::Error>
-    where I: Iterator,
-          <I as Iterator>::Item: Borrow<VertexId>
+    where I: IntoIterator,
+          <I as IntoIterator>::Item: Borrow<VertexId>
 {
     let futs = i
+        .into_iter()
         .map(| v | vertex_to_file(v.borrow()))
-        .map(move | f | write_file_in_dir(path.as_ref(), f).and_then(| _ | Ok(()))) ;
+        .map(move | f | write_file_in_dir(path.as_ref(), f).map(| _ | () )) ;
 
     futures::future::join_all(futs)
 }
 
-fn write_file(file: File) -> impl Future {
-    let path = Path::new("./foo");
+fn store_graph_vertices(path: PathBuf, graph: &DirectedGraph) -> impl Future<Item=Vec<()>, Error = io::Error> {
+    let vertices: Vec<VertexId> = graph
+        .vertices()
+        .map(| v | *v)
+        .collect();
 
-    tokio_fs::create_dir_all(path)
-        .and_then(move | _ | {
-            let path = path.join(&file.name);
-            tokio_fs::write(path, file.content)
-        })
+    tokio_fs::create_dir_all(path.clone()).and_then(move | _ | {
+        write_all_vertices_to_files(path, vertices)
+    })
 }
 
 #[cfg(test)]
@@ -79,57 +96,70 @@ mod test {
     use histo_graph_core::graph::graph::VertexId;
     use super::{File,
                 vertex_to_file,
-                write_file,
                 write_all_vertices_to_files};
     use futures::future::Future;
+    use tokio::runtime::Runtime;
     use std::path::{Path, PathBuf};
+    use histo_graph_core::graph::directed_graph::DirectedGraph;
+    use crate::file_storage::{store_graph_vertices, write_file_in_dir, read_file_in_dir, file_to_vertex};
 
     #[test]
     fn test_hash() {
-        let File{content: _, name} = vertex_to_file(&VertexId(27));
+        let File{content: _, hash} = vertex_to_file(&VertexId(27));
 
-        assert_eq!(name, "4d159113222bfeb85fbe717cc2393ee8a6a85b7ce5ac1791c4eade5e3dd6de41")
+        assert_eq!(hash.to_string(), "4d159113222bfeb85fbe717cc2393ee8a6a85b7ce5ac1791c4eade5e3dd6de41")
     }
 
     #[test]
-    fn test_write_vertex() {
-        let file = vertex_to_file(&VertexId(27));
+    fn test_write_and_read_vertex() -> Result<(), std::io::Error> {
+        let vertex = VertexId(18);
 
-        let f = write_file(file)
-            .map(| _ | ())
-            .map_err(| _ | eprintln!("Error"));
+        let file = vertex_to_file(&vertex);
+        let hash = file.hash;
 
-        tokio::run(f);
+        let path: PathBuf = Path::new("../target/test/store/").into();
+
+        let f = write_file_in_dir(&path, file)
+            .and_then(move | _ | read_file_in_dir(&path, hash));
+
+        let mut rt = Runtime::new()?;
+        let file = rt.block_on(f)?;
+
+        let result = file_to_vertex(&file).unwrap();
+
+        assert_eq!(result, vertex);
+
+        Ok(())
     }
 
-    /*fn the_future() -> impl Future<Item = (), Error = ()> {
-        let vertices = vec!{VertexId(1), VertexId(2), VertexId(3), VertexId(4)};
-        let iter = vertices.iter();
-
-        let path: PathBuf = Path::new("foo/").into();
-
-        let f = write_all_vertices_to_files(path, iter);
-
-        let f = f
-            .and_then(| _ | { println!("Done"); Ok(()) })
-            .map_err(| _ | eprintln!("Error"));
-
-        f
-    }*/
-
     #[test]
-    fn test_write_vertices() {
+    fn test_write_vertices() -> Result<(), std::io::Error> {
         let vertices = vec!{VertexId(1), VertexId(2), VertexId(3), VertexId(4)};
 
-        let path: PathBuf = Path::new("foo/").into();
+        let path: PathBuf = Path::new("../target/test/store/").into();
 
         let f = write_all_vertices_to_files(path, vertices.into_iter());
 
-        let f = f
-            .and_then(| _ | { println!("Done"); Ok(()) })
-            .map_err(| _ | eprintln!("Error"));
+        let mut rt = Runtime::new()?;
+        rt.block_on(f)?;
 
+        Ok(())
+    }
 
-        tokio::run(f);
+    #[test]
+    fn test_store_graph_vertices() -> Result<(), std::io::Error> {
+        let mut graph = DirectedGraph::new();
+        graph.add_vertex(VertexId(27));
+        graph.add_vertex(VertexId(28));
+        graph.add_vertex(VertexId(29));
+
+        let path: PathBuf = Path::new("../target/test/store/").into();
+
+        let f = store_graph_vertices(path, &graph);
+
+        let mut rt = Runtime::new()?;
+        rt.block_on(f)?;
+
+        Ok(())
     }
 }
