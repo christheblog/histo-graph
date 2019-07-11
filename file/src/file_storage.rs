9f8c7b26f78afd4ce13c7fb0e26d76e2aa69ac1e
David Peklak <dpeklak@gmail.com>
2019-07-11 17:56:16 +0200

    write and read edge

--- file/src/file_storage.rs
+++ file/src/file_storage.rs
@@ -1,5 +1,5 @@
 use histo_graph_core::graph::{
-    graph::VertexId,
+    graph::{VertexId, Edge},
     directed_graph::DirectedGraph,
 };
 
@@ -44,9 +44,31 @@ struct File {
     hash: Hash,
 }
 
+/// A HashEdge respresents an edge by the hashes of the vertices it is connected to.
+#[derive(Serialize, Deserialize)]
+struct HashEdge{
+    from: Hash,
+    to: Hash,
+}
+
 fn vertex_to_file(vertex_id: &VertexId) -> File {
     // serialize the vertex_id
     let content: Vec<u8> = bincode::serialize(&vertex_id.0).unwrap();
+    let hash: Hash = (&content).into();
+
+    File {
+        content,
+        hash,
+    }
+}
+
+fn edge_to_file(edge: &Edge) -> File {
+    let File { hash: v_hash_0, ..} = vertex_to_file(&edge.0);
+    let File { hash: v_hash_1, ..} = vertex_to_file(&edge.1);
+
+    let hash_edge = HashEdge { from: v_hash_0,  to: v_hash_1};
+
+    let content: Vec<u8> = bincode::serialize(&hash_edge).unwrap();
     let hash: Hash = (&content).into();
 
     File {
@@ -69,6 +91,11 @@ fn hash_vec_to_file(hash_vec: &Vec<Hash>) -> File {
 fn file_to_vertex(file: &File) -> Result<VertexId> {
     let id: u64 = bincode::deserialize(file.content.as_ref())?;
     Ok(VertexId(id))
+}
+
+fn file_to_hash_edge(file: &File) -> Result<HashEdge> {
+    bincode::deserialize(file.content.as_ref())
+        .map_err(Into::into)
 }
 
 fn file_to_hash_vec(file: &File) -> Result<Vec<Hash>> {
@@ -122,9 +149,8 @@ fn write_vertex_hash_vec_file(base_path: PathBuf, hash_vec: Vec<Hash>) -> impl F
         .map( move | _ | hash)
 }
 
-/// Writes the vertices of a graph. Returns Future of the hash of the vertex vector file.
-///
-///
+/// Writes the vertices of a graph.
+/// Returns the hash of the vertex vector file.
 fn write_graph_vertices(base_path: PathBuf, graph: &DirectedGraph) -> impl Future<Item = Hash, Error = io::Error> {
     let vertices: Vec<VertexId> = graph
         .vertices()
@@ -138,6 +164,15 @@ fn write_graph_vertices(base_path: PathBuf, graph: &DirectedGraph) -> impl Futur
         .and_then(move | hash_vec |
             write_vertex_hash_vec_file(base_path, hash_vec)
         )
+}
+
+/// Writes an edge to a file in the directory specified by `dir_path`.
+/// Returns the hash of the file.
+fn write_edge_to_file(dir_path: PathBuf, edge: &Edge) -> impl Future<Item = Hash, Error = io::Error> {
+    let file = edge_to_file(edge);
+    let hash = file.hash;
+    write_file_in_dir(&dir_path, file)
+        .map(move | _ | hash)
 }
 
 fn read_file_in_dir(dir_path: &Path, hash: Hash) -> impl Future<Item = File, Error = io::Error> {
@@ -193,6 +228,30 @@ fn read_graph_vertices(base_path: PathBuf, hash: Hash, mut graph: DirectedGraph)
                 graph.add_vertex(v);
             }
             Ok(graph)
+        })
+}
+
+fn read_hash_edge(dir_path: PathBuf, hash: Hash) -> impl Future<Item = HashEdge, Error = Error> {
+    read_file_in_dir(&dir_path, hash)
+        .map_err(Into::into)
+        .and_then(|file| file_to_hash_edge(&file))
+}
+
+fn read_edge(base_path: PathBuf, hash: Hash) -> impl Future<Item = Edge, Error = Error> {
+    let edge_path = base_path.join("edge");
+    let vertex_path = base_path.join("vertex");
+
+    read_hash_edge(edge_path, hash)
+        .and_then(move |HashEdge { from, to}| {
+            let from_fut = read_file_in_dir(&vertex_path, from)
+                .map_err(Into::into)
+                .and_then(|file| file_to_vertex(&file));
+            let to_fut = read_file_in_dir(&vertex_path, to)
+                .map_err(Into::into)
+                .and_then(|file| file_to_vertex(&file));
+
+            from_fut.join(to_fut)
+                .map(|(v0, v1)| Edge(v0, v1))
         })
 }
 
@@ -269,6 +328,28 @@ mod test {
         let result_graph = rt.block_on(f)?;
 
         assert_eq!(graph, result_graph);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_and_read_edge() -> Result<()> {
+        let vertices = vec![VertexId(42), VertexId(43)];
+        let edge = Edge(VertexId(42), VertexId(43));
+
+        let path: PathBuf = Path::new("../target/test/store/").into();
+        let edge_path: PathBuf = path.join("edge");
+
+        let f = write_all_vertices_to_files(path.clone(), vertices)
+            .and_then({ let edge_path = edge_path.clone(); move | _ | tokio_fs::create_dir_all(edge_path)})
+            .and_then(move | _ | write_edge_to_file(edge_path, &edge))
+            .map_err(Into::into)
+            .and_then(move |hash| read_edge(path, hash));
+
+        let mut rt = Runtime::new()?;
+        let result_edge = rt.block_on(f)?;
+
+        assert_eq!(edge, result_edge);
 
         Ok(())
     }
