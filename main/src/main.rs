1cbb0d8c49d24dd52a89677fc496a9cae42a52a4
David Peklak <dpeklak@gmail.com>
2019-07-16 21:32:53 +0200

    show graph in main

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,4 +1,9 @@
 use clap::{App, SubCommand};
+use histo_graph_file::file_storage::load_graph;
+use std::path::{PathBuf, Path};
+use histo_graph_serde::directed_graph_serde::DirectedGraphSer;
+use std::ffi::OsString;
+use tokio::runtime::Runtime;
 
 fn main() {
     let matches = App::new("histo-graph")
@@ -10,6 +15,13 @@ fn main() {
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("show") {
-        println!("Show the graph");
+        let base_dir: PathBuf = Path::new("../target/test/store/").into();
+        let f = load_graph(base_dir, &OsString::from("laurengraph"));
+
+        let mut rt = Runtime::new().unwrap();
+        let graph = rt.block_on(f).unwrap();
+        let ser: DirectedGraphSer = (&graph).into();
+        let str = serde_json::to_string(&ser).unwrap();
+        println!("{}", str);
     }
 }
